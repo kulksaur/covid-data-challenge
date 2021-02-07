e7aeb0a55d56b2a33be53c7023a49acf0dd32ddc
Saurabh <kulkarni.saur@husky.neu.edu>
2021-02-07 03:52:21 -0500

    Reverting mocking in schedule cron script

--- scheduleCron.py
+++ scheduleCron.py
@@ -4,9 +4,17 @@ import pathlib
 CURRENT_DIR = pathlib.Path().absolute()
 
 cronLogFilePath = pathlib.Path(str(CURRENT_DIR)) / "cron.log"
-executableFilePath = pathlib.Path(str(CURRENT_DIR)) / "ImportCovidDataTaskExecutable"
-commandToRunForCron = f"{executableFilePath} >> {cronLogFilePath} 2>&1"
+
+# For Unix/Linux Environments
+# executableFilePath = pathlib.Path(str(CURRENT_DIR)) / "ImportCovidDataTaskExecutable"
+# commandToRunForCron = f"{executableFilePath} >> {cronLogFilePath} 2>&1"
+
+# For Any Environments
+pythonDirectory = pathlib.Path(str(CURRENT_DIR)) / "venv" / "bin" / "python"
+mainSourceScriptPath = pathlib.Path(str(CURRENT_DIR)) / "source" / "importDataTask.py"
+commandToRunForCron = f"{pythonDirectory} {mainSourceScriptPath} >> {cronLogFilePath} 2>&1"
+
 cron = CronTab(user=True)
 job = cron.new(command=commandToRunForCron)
-job.minute.every(1)
+job.setall("0 9 * * *")
 cron.write()
